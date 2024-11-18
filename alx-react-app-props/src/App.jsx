433318e629302b8524058b0b83d45fb10304d5d2
Chimzurum Crowm <ezulikecrown@gmail.com>
2024-11-18 12:39:28 +0100

    Modified alx-react-app-props

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,43 +1,27 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-import WelcomeMessage from './components/WelcomeMessage'
-import Header from './Header'
-import MainContent from './MainContent'
-import Footer from './Footer'
-import UserProfile from './components/UserProfile'
+import UserContext from './components/UserContext';
+import ProfilePage from './ProfilePage';
+import UserProfile from './components/UserProfile';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  const abNumb = '347893478e578'
+
 
   return (
     <>
       <div>
-        <WelcomeMessage />
-        <Header />
-        <MainContent />
-        <Footer />
-        <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <UserContext.Provider value={{userData:userData, abNumb:abNumb}}>
+        <UserProfile />
+        </UserContext.Provider>
+      
+      <UserContext.Provider value={{userData:userData, abNumb:abNumb}}>
+      
+        <ProfilePage />
+      </UserContext.Provider>
+       
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
