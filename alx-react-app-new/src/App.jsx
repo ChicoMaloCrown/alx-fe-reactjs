5fcef28e5c66b0127d6e69bc27f45a684c3c1628
Chimzurum Crowm <ezulikecrown@gmail.com>
2024-11-18 12:09:07 +0100

    Added counter.jsx to components

--- alx-react-app-new/src/App.jsx
+++ alx-react-app-new/src/App.jsx
@@ -7,6 +7,7 @@ import Header from './Header'
 import MainContent from './MainContent'
 import Footer from './Footer'
 import UserProfile from './components/UserProfile'
+import Counter from './components/Counter'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -19,6 +20,7 @@ function App() {
         <MainContent />
         <Footer />
         <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+        <Counter />
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
